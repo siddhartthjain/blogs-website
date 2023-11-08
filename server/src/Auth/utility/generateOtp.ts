ff8802b34816d5660c3218cedbdcfe3d4358161d
siddharth-velotio <siddharth.jain@velotio.com>
2023-11-08 10:26:27 +0530

    additional commit

--- server/src/Auth/utility/generateOtp.ts
+++ server/src/Auth/utility/generateOtp.ts
@@ -1,9 +1,9 @@
 export default function generateOTP(length: number) {
-    const characters = '0123456789';
-    let OTP = '';
-    for (let i = 0; i < length; i++) {
-        const index = Math.floor(Math.random() * characters.length);
-        OTP += characters[index];
-    }
-    return OTP;
+  const characters = "0123456789";
+  let OTP = "";
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * characters.length);
+    OTP += characters[index];
+  }
+  return OTP;
 }
